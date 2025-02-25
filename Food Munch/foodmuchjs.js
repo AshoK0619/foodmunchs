1e0598ec71ea671741e306aea421c5828ceeae50
AshoK0619 <triosharma545@gmail.com>
2025-02-25 20:16:48 +0530

    Add files via upload

---
+++ Food Munch/foodmuchjs.js
@@ -0,0 +1,11 @@
+function goToViewMenu(){
+    window.location.href = "viewmenu.html"
+}
+
+function whyChooseUs(){
+    window.location.href = "choose.html"
+}
+
+function foodMunchHome(){
+    window.location.href = "foodmunch.html"
+}
